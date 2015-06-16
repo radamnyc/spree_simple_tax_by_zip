14e687320d39ce1700991729828c08ab1746f62a
adam <adam@splickit.com>
2015-06-16 08:29:42 -0600

    removing extra logging

--- app/models/spree/tax_rate_decorator.rb
+++ app/models/spree/tax_rate_decorator.rb
@@ -1,13 +1,8 @@
 Spree::TaxRate.class_eval do
 
   def self.adjust(order, items)
-    Rails.logger.debug "********************* HERE I AM *************************"
-    Rails.logger.debug "caller for adjust: #{caller[0]}"
-    Rails.logger.debug "submitted items are: #{items}"
     pre_filtered_rates = match(order.tax_zone)
-    Rails.logger.debug "we have the rates: #{pre_filtered_rates.inspect}"
     rates = filter_on_tax_rate_zip_if_applicable(order.tax_address.zipcode,pre_filtered_rates)
-    Rails.logger.debug "we have the FILTERED rates: #{rates.inspect}"
     tax_categories = rates.map(&:tax_category)
     relevant_items, non_relevant_items = items.partition { |item| tax_categories.include?(item.tax_category) }
     Spree::Adjustment.where(adjustable: relevant_items).tax.destroy_all # using destroy_all to ensure adjustment destroy callback fires.
@@ -28,11 +23,9 @@ Spree::TaxRate.class_eval do
 
 
   def self.filter_on_tax_rate_zip_if_applicable(order_zip,rates)
-    Rails.logger.debug "submitted zipcode is: #{order_zip}"
-    Rails.logger.debug "submitted rates are: #{rates.inspect}"
+    Rails.logger.debug "submitted zipcode is: #{order_zip},  submitted rates are: #{rates.inspect}"
     zip_filtered_rates = Array.new
     rates.each do |rate|
-      Rails.logger.debug "examining rate: #{rate.inspect}"
       if rate.zip_codes.blank?
         zip_filtered_rates.push(rate)
       elsif does_zip_match?(order_zip,rate.zip_codes)
