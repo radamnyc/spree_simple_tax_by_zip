568c9197eb6324426a7a90497bcf0cc22b4b0e3a
adam <adam@splickit.com>
2015-06-15 22:12:34 -0600

    trying to figure ANYTHING out that works

--- app/models/spree/tax_rate_decorator.rb
+++ app/models/spree/tax_rate_decorator.rb
@@ -1,16 +1,17 @@
 Spree::TaxRate.class_eval do
 
   def self.match(order_tax_zone)
+    puts "******* about to get caller *************"
+    puts "caller for match: #{caller[0]}"
+    puts "******* end getting caller *************"
     return [] unless order_tax_zone
 
     potential_rates = potential_rates_for_zone(order_tax_zone)
     rates = potential_rates.includes(zone: { zone_members: :zoneable }).load.select do |rate|
+      # Why "potentially"?
+      # Go see the documentation for that method.
       rate.potentially_applicable?(order_tax_zone)
     end
-
-    puts "********** HERE I AM *************"
-    puts "order tax zone: #{order_tax_zone.inspect}"
-    puts "rate: #{rates.inspect}"
 
     # Imagine with me this scenario:
     # You are living in Spain and you have a store which ships to France.
@@ -29,13 +30,13 @@ Spree::TaxRate.class_eval do
       rate.included_in_price? &&
           (rates - [rate]).map(&:tax_category).include?(rate.tax_category)
     end
-    puts "********** HERE I AM *************"
-    puts "rate: #{rates.inspect}"
-
   end
 
   def self.adjust(order, items)
-    rates = match(order.tax_zone)
+    puts "********************* HERE I AM *************************"
+    puts "caller for adjust: #{caller[0]}"
+    pre_filtered_rates = match(order.tax_zone)
+    rates = filter_on_tax_rate_zip_if_applicable(order.tax_address.zipcode,pre_filtered_rates)
     tax_categories = rates.map(&:tax_category)
     relevant_items, non_relevant_items = items.partition { |item| tax_categories.include?(item.tax_category) }
     Spree::Adjustment.where(adjustable: relevant_items).tax.destroy_all # using destroy_all to ensure adjustment destroy callback fires.
@@ -55,16 +56,25 @@ Spree::TaxRate.class_eval do
   end
 
 
-  def potentially_applicable?(order_tax_zone)
-    # If the rate's zone matches the order's tax zone, then it's applicable.
-    self.zone == order_tax_zone ||
-        # If the rate's zone *contains* the order's tax zone, then it's applicable.
-        self.zone.contains?(order_tax_zone) ||
-        # 1) The rate's zone is the default zone, then it's always applicable.
-        (self.included_in_price? && self.zone.default_tax)
+  def self.filter_on_tax_rate_zip_if_applicable(order_zip,rates)
+    #puts "submitted rates are: #{rates.inspect}"
+    zip_filtered_rates = Array.new
+    rates.each do |rate|
+      #puts "examining rate: #{rate.inspect}"
+      if rate.zip_codes.blank?
+        zip_filtered_rates.push(rate)
+      elsif does_zip_match?(order_zip,rate.zip_codes)
+        zip_filtered_rates.push(rate)
+      else
+        #puts "we are skipping adding because the zip doesn't match"
+      end
+    end
+    zip_filtered_rates
   end
 
 
-
+  def self.does_zip_match?(order_zip,rate_zip_list)
+    rate_zip_list.include? order_zip
+  end
 
 end
